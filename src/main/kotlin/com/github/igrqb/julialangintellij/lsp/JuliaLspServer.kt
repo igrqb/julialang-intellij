2731df463aa252b87807e39cab736d6d44ef2c13
igrqb <100824876+igrqb@users.noreply.github.com>
2024-02-28 11:57:47 +1000

    added Julia LSP server

---
+++ src/main/kotlin/com/github/igrqb/julialangintellij/lsp/JuliaLspServer.kt
@@ -0,0 +1,20 @@
+package com.github.igrqb.julialangintellij.lsp
+
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServerSupportProvider
+import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+
+internal class JuliaLspServer : LspServerSupportProvider {
+  override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter) {
+    if (file.extension == "jl") {
+      serverStarter.ensureServerStarted(JuliaLspServerDescriptor(project))
+    }
+  }
+}
+
+private class JuliaLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Julia") {
+  override fun isSupportedFile(file: VirtualFile) = file.extension == "jl"
+  override fun createCommandLine() = GeneralCommandLine("julia", "-e", "\"using LanguageServer; runserver()\"")
+}
